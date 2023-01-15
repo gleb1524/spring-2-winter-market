d65a511c1f370cee5fab914eae7ce6c94bd5022f
gleb <gleb1524@yandex.ru>
2023-01-15 16:03:00 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/karaban/springWinterMarket/Spring2WinterMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.karaban.springWinterMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2WinterMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2WinterMarketApplication.class, args);
+	}
+
+}
